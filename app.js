a73f96e7975e173f526ce835ba24ec3621523e23
rakpcodes <rakpcodes@gmail.com>
2023-02-22 03:24:06 -0500

    created app namespace, passed intial API response and performed successful 2nd call, added pseudo code

--- app.js
+++ app.js
@@ -2,28 +2,68 @@
 - Make successful API call for pokemon list
 - Create function to select random pokemon index from list
 - Make 2nd call to pokemon endpoint using name as string param
-- Gather and store stats, create functions to calculate rounded values for HP, Attack and Defense
+- Gather the following data:
+    - name
+    - sprites.other.official-artwork.front_default
+    - stats
+        - HP = stats[0].base_stat * 1.5 rounded to nearest 10 
+        - Attack = stats[1].base_stat + stats[3].base_stat / rounded to nearest 10
+        - Defense = stats[2].base_stat + stats[4].base_stat / rounded to nearest 10
+        - Speed = stats[5].base_stat rounded to nearest 10
+    - types
+        - Main = types[0].type.name
+        - Second = types[1].type.name
+    -moves
+        - moves[rndm] * 4 (conditionals to see if there are less than 4 available)
+        - chance.js for picking 4 random unique indexes
 
-
+- Create function to update html on page
 
 */
 
-const natDexRndm = Math.floor(Math.random())
 
 
+const app = {};
+const natDexRndm = Math.floor(Math.random() * 252);
 
-$.ajax({
-    url: "https://pokeapi.co/api/v2/pokemon",
-    data: {
-        limit: "649",
-    },
-    dataType: "json",
-    success: function (response) {
 
-        console.log(response)
-        const pkmnName = response.results[0].name
+// First API Call to retrieve Random Pokemon
+app.getPkmn = function () {
 
-        $(".pkmn-card-name").text(pkmnName)
+    $.ajax({
 
-    }
-});
+        url: "https://pokeapi.co/api/v2/pokemon",
+        data: {
+            limit: "251",
+        },
+        dataType: "json",
+
+        success: function (response) {
+            console.log(response.results[natDexRndm].name)
+            app.getPkmnInfo(response.results[natDexRndm].name)
+        }
+    });
+}
+
+// Second API Call to retrieve Pokmon Information
+app.getPkmnInfo = function (pkmnName) {
+
+    $.ajax({
+        url: `https://pokeapi.co/api/v2/pokemon/${pkmnName}`,
+        dataType: "json",
+
+        success: function (response) {
+            console.log(response)
+        }
+    });
+}
+
+
+app.init = () => {
+    app.getPkmn();
+};
+
+$(function () {
+    app.init()
+})
+
