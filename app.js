e328fcb98e17c72d8c38a67dec123ec8fb6d9c95
rakpcodes <rakpcodes@gmail.com>
2023-03-15 18:12:32 -0400

    added media up to tab-land

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ menuButton.click(function () {
     menuButton.addClass("disappear");
     navModal.removeClass("hide")
     body[0].style.overflow = "hidden"
-    body[0].style.paddingRight = "1.5rem"
+    body[0].style.paddingRight = "1.6rem"
 })
 
 btnBackNav.click(function () {
@@ -166,6 +166,7 @@ app.getPkmn = function () {
 app.addCardData = (response) => {
 
     // Add Pokemon Name
+    $(".pkmn-card-name")[0].style.width = "max-content"
     const pkmnName = response.name
     $(".pkmn-card-name").text(pkmnName)
 
@@ -295,6 +296,9 @@ app.addCardData = (response) => {
     }
 
     // Update Stats
+
+    $(".stats")[0].style.top = "50%"
+
     const statsHP = Math.round((response.stats[0].base_stat / 10) * 1.5) * 10
     const statsAttack = Math.round(((response.stats[1].base_stat + response.stats[3].base_stat) / 2) / 10) * 10
     const statsDefense = Math.round(((response.stats[2].base_stat + response.stats[4].base_stat) / 2) / 10) * 10
